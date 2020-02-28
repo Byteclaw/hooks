8544586706a6915e29096a0778a3478ea8cfd99a
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-02-28 20:46:28 +0100

    fix(hooks): fix missing export of use-static-callback

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@byteclaw/use-event-emitter';
 export * from '@byteclaw/use-overflow-scroll-position';
 export * from '@byteclaw/use-prompt';
+export * from '@byteclaw/use-static-callback';
 export * from '@byteclaw/use-unique-id';
